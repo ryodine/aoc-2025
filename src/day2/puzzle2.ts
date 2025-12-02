514e44ba71fd1934df08ef5dee43cf7d8b2c0837
Ryan Johnson <ryan@johnsonweb.us>
2025-12-02 00:44:34 -0500

    alt faster / not regex version

--- src/day2/puzzle2.ts
+++ src/day2/puzzle2.ts
@@ -29,10 +29,32 @@ function makeRangeWalk(checkers: Checker[]): (range: Range) => Set<number> {
 
 function hasRepeatingPattern(n: number): boolean {
   const nStr = n.toString();
-  return /^([0-9]+)\1+$/gm.test(nStr);
+  const nStrLen = nStr.length;
+  const nStrHalfLen = Math.floor(nStrLen / 2);
+  // Slower, simpler version:
+  // return /^([0-9]+)\1+$/gm.test(nStr);
+
+  // Strategy: pick larger and larger slices from the front of the string,
+  // and see if repeating that slice makes up the whole string.
+  // e.g. for 123123123, try "1", "12", "123", etc.
+  // Skip sizes that don't evenly divide the string length.
+  ol: for (let patternSz = 1; patternSz <= nStrHalfLen; patternSz++) {
+    if (nStr.length % patternSz !== 0) {
+      continue;
+    }
+    const pattern = nStr.slice(0, patternSz);
+    for (let i = patternSz; i < nStrLen; i += patternSz) {
+      if (nStr.slice(i, i + patternSz) !== pattern) {
+        continue ol;
+      }
+    }
+    return true;
+  }
+  return false;
 }
 
 function puzzle2(input: string[]): number {
+  const start = performance.now();
   const sum = input
     .map(toRange)
     .map(makeRangeWalk([hasRepeatingPattern]))
@@ -41,6 +63,8 @@ function puzzle2(input: string[]): number {
     }, new Set<number>())
     .values()
     .reduce((sum, id) => sum + id, 0);
+  const end = performance.now();
+  console.log(`(took ${end - start}ms)`);
   return sum;
 }
 
